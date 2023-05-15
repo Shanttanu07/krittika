6ecbc6f4535d74f3e0fd460c46969fdd9a8a3014
Shanttanu07 <126013163+Shanttanu07@users.noreply.github.com>
2023-05-15 14:31:12 +0530

    partA

--- webscraper.py
+++ webscraper.py
@@ -5,41 +5,36 @@ import pandas as pd
 from time import sleep
 
 pd.set_option('display.max_colwidth', 255)
-# main_site='https://itc.gymkhana.iitb.ac.in/wncc'
-# main_cont=requests.get(main_site).content
 
-# database=[]
+database=[]
+year=[2019,2020,2021,2022]
 
-# for i in range(200,350):
-#     url='https://itc.gymkhana.iitb.ac.in/wncc/soc/projects/2023/project{}.html'.format(i)
-#     project={"Title": None, "Mentor": None, "Mentees": None, "Project_ID": None}
-#     print(url)
-#     request_content=requests.get(url).content
-#     if (request_content==main_cont):
-#         continue
-#     soup=BeautifulSoup(request_content,'html.parser')
+for i in year:
+    team=[]
+    url='https://krittikaiitb.github.io/team/'
+    request_content=requests.get(url).content
+    soup=BeautifulSoup(request_content,'html.parser')
 
-#     title=soup.select_one('.project-title').text
-#     project['Title']=title
-#     mentors_block=soup.select('body > div.container-fluid > div > div.col-sm-10.col-md-8 > div:nth-child(4) > ul:nth-child(5) .lead')
-#     str=''
-#     for name in mentors_block:
-#         str=str+name.text.strip(" : ")+' / '
-#     project['Mentor']=str.strip(' / ')
-#     menteecount=soup.select_one('body > div.container-fluid > div > div.col-sm-10.col-md-8 > div:nth-child(4) > ul:nth-child(7) > li .lead').text.strip()
-#     project['Mentees']=menteecount
-#     project['Project_ID']=i
-#     database.append(project)
-#     sleep(1)
+    name=soup.select(('#team-{} h5').format(i))
+    desig=soup.select(('#team-{} .card-text').format(i))
+    for p in range(len(name)):
+        member={"Name": None, "Designation": None}
+        member['Name']=name[p].text
+        member['Designation']=desig[p].text
+        team.append(member)
+    database.append(team)
 
-# with open('database.json', 'w') as f:
+for k in database:
+    print(k)
+
+# with open('kritika_database.json', 'w') as f:
 #     json.dump(database, f)
 #     f.close()
 
-with open('database.json', 'r') as f:
-    data = json.load(f)
+# with open('kritika_database.json', 'r') as f:
+#     data = json.load(f)
 
-df=pd.read_json(open('database.json','r'))
-# df.head(15)
-# df.tail(15)
-print(df.head(50))
+# df=pd.read_json(open('kritika_database.json','r'))
+# # df.head(15)
+# # df.tail(15)
+# print(df.head(10))
